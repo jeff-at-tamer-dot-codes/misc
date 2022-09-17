0f39956b0e76afa83738eefcf150f68f7ef4dd2c
Jeff Tamer <jeff@Max-JeffBook-Pro.local>
2022-09-17 10:25:55 -0700

    playing with visualization in racket

--- euler189.rkt
+++ euler189.rkt
@@ -1,5 +1,8 @@
 #lang slideshow
 
+; Author: Naked Jeff <jeff@tamer.codes>
+; See https://projecteuler.net/problem=189
+
 (require memo)
 
 (define-syntax assert-nonnegative-integer
@@ -50,3 +53,41 @@
   (recurse num-triangles 0 0 0 0 0 1))))
 
 (define count-colorings count-colourings)
+
+; The above code solves the problem. The following code explores visualization:
+
+(require racket/draw)
+(require racket/gui)
+
+(define SIZE 16)
+(define HEIGHT (* SIZE (sqrt 3)))
+(define CENTER 200)
+(define target (make-bitmap (* 2 CENTER) (* 2 CENTER)))
+(define dc (new bitmap-dc% [bitmap target]))
+
+(define (triangle op x y color)
+  (define path (new dc-path%))
+  (define new-y (op y HEIGHT))
+  (send path move-to x y)
+  (send path line-to (- x SIZE) new-y)
+  (send path line-to (+ x SIZE) new-y)
+  (send path line-to x y)
+  (send dc set-brush color 'solid)
+  (send dc draw-path path))
+
+(define (triangle-up x y color) (triangle + x y color))
+(define (triangle-down x y color) (triangle - x y color))
+
+(triangle-up CENTER 0 "RED")
+
+(triangle-up (- CENTER SIZE) HEIGHT "BLUE")
+(triangle-down CENTER (* 2 HEIGHT) "GREEN")
+(triangle-up (+ CENTER SIZE) HEIGHT "BLUE")
+
+(triangle-up   (+ CENTER (* -2 SIZE)) (* 2 HEIGHT) "GREEN")
+(triangle-down (+ CENTER (* -1 SIZE)) (* 3 HEIGHT) "RED")
+(triangle-up   (+ CENTER (*  0 SIZE)) (* 2 HEIGHT) "BLUE")
+(triangle-down (+ CENTER (*  1 SIZE)) (* 3 HEIGHT) "RED")
+(triangle-up   (+ CENTER (*  2 SIZE)) (* 2 HEIGHT) "BLUE")
+
+(define my-triangle-image (make-object image-snip% target))
